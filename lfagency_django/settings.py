4567f5cfb34a4b58acfc312578c5104469cdf57b
hocdu91 <hb91220@gmail.com>
2021-09-08 13:34:54 +0200

    Update settings.py

--- lfagency_django/settings.py
+++ lfagency_django/settings.py
@@ -139,7 +139,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
