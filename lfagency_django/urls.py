85bd7582b775456e6f238dce32ba6bef75636618
hocdu91 <hb91220@gmail.com>
2021-09-08 23:33:23 +0200

    media debug false

--- lfagency_django/urls.py
+++ lfagency_django/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 from lfapp import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 
 
@@ -27,6 +28,8 @@ urlpatterns = [
     url(r'^$', views.index),
     url(r'^lfapp/', include('lfapp.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
