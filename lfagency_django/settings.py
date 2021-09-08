0c015f19500647686245657824fe6d9ece3154cf
hocdu91 <hb91220@gmail.com>
2021-09-08 14:37:39 +0200

    Update settings.py

--- lfagency_django/settings.py
+++ lfagency_django/settings.py
@@ -139,14 +139,13 @@ if os.environ.get('ENV') == 'PRODUCTION':
     PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
         os.path.join(PROJECT_ROOT, 'static'),
     )
-    MEDIAFILES_DIRS = (MEDIA_ROOT)
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+    ALLOWED_HOSTS = ["lfagency.herokuapp.com"]
     
 else:
     DEBUG= True 
